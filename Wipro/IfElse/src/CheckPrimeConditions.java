52efccbaaf2d2efe24524ad2e2884f61ed8df501
Solleti Rohith <42320925+rohithraj99@users.noreply.github.com>
2020-03-28 12:19:20 +0530

    Update CheckPrimeConditions.java

--- Wipro/IfElse/src/CheckPrimeConditions.java
+++ Wipro/IfElse/src/CheckPrimeConditions.java
@@ -1,49 +1,31 @@
 import java.util.Scanner;
-
-/**
- * 
- */
-
-/**
- * @author parsh
- *
- */
-public class CheckPrimeConditions {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+public class primecheck 
+{
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+			int temp;
+			boolean isPrime=true;
+			Scanner scan= new Scanner(System.in);
+			System.out.println("Enter any number:");
+			//capture the input in an integer
+			int num=scan.nextInt();
+		        scan.close();
+			for(int i=2;i<=num/2;i++)
+			{
+		           temp=num%i;
+			   if(temp==0)
+			   {
+			      isPrime=false;
+			      break;
+			   }
+			}
+			//If isPrime is true then the number is prime else not
+			if(isPrime)
+			   System.out.println(num + " is a Prime Number");
+			else
+			   System.out.println(num + " is not a Prime Number");
 		
-		try {
-		@SuppressWarnings("resource")
-		Scanner reader = new Scanner(System.in);
-		int number =Integer.parseInt(reader.next());
-        boolean check = false;
-        if(number==0||number==1) {
-        	System.out.print(number+" is neither prime nor composite");
-        }
-        else
-        {
-        	for(int i = 2; i <= number/2; ++i)
-            {
-                if(number % i == 0)
-                {
-                    check = true;
-                    break;
-                }
-            }
-            if (!check)
-                System.out.println(number + " is a prime number.");
-            else
-                System.out.println(number + " is not a prime number.");
-        }
-        
-		}
-		catch(Exception e) {
-			System.out.print("Please Enter a Integer Number");
-		}
-
 	}
 
 }
