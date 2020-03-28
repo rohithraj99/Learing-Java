4946a648cd2989cc70096a7fbe4688b65c0d5031
Solleti Rohith <42320925+rohithraj99@users.noreply.github.com>
2020-03-28 12:22:48 +0530

    Update FirstFive.java

--- Wipro/IfElse/src/FirstFive.java
+++ Wipro/IfElse/src/FirstFive.java
@@ -1,16 +1,6 @@
-/**
- * 
- */
 
-/**
- * @author parsh
- *
- */
 public class FirstFive {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int count=0;
