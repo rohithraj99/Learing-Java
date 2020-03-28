794d354bf79eb2119166a15848cbd32e48107ec7
Solleti Rohith <42320925+rohithraj99@users.noreply.github.com>
2020-03-28 12:39:56 +0530

    Update SortArray.java

--- Wipro/Arrays/src/SortArray.java
+++ Wipro/Arrays/src/SortArray.java
@@ -1,18 +1,7 @@
 import java.util.Arrays;
 
-/**
- * 
- */
-
-/**
- * @author parsh
- *
- */
 public class SortArray {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int inputArray[] = new int[]{10, 11, 88, 2, 12, 120};
